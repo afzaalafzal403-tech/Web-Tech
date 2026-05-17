0d10d32c103bbb975aa20fe212830962e8d71991
Afzaal Afzal <afzaalafzal403@gmail.com>
2026-05-17 21:48:42 +0500

    Add files via upload

---
+++ Lab Assignment 4/models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    min: 0,
+    max: 5,
+    default: 0
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  image: {
+    type: String,
+    default: '/uploads/default-product.svg'
+  }
+});
+
+productSchema.virtual('imageUrl').get(function() {
+  const imagePath = this.image || '/uploads/default-product.svg';
+  return imagePath === '/uploads/default-product.png' ? '/uploads/default-product.svg' : imagePath;
+});
+
+productSchema.set('toObject', { virtuals: true });
+productSchema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('Product', productSchema);
